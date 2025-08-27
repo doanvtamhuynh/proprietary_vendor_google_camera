c67790807130cabf45bea81f6e5f7f429d904caa
doanvtamhuynh <huynhdoan.2132@gmail.com>
2025-08-27 09:52:46 +0700

    Initialize

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Fetching latest GoogleCamera.apk ..."
+curl -L --user-agent 'Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0' --output vendor/google/camera/proprietary/product/priv-app/GoogleCamera/GoogleCamera.apk https://d.apkpure.net/b/APK/com.google.android.GoogleCamera?version=latest
